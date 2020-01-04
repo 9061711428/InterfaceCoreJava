1d6ce4e6f9fe7d8b4415117f62963221144b6f9f
s@1996 <54677210+9061711428@users.noreply.github.com>
2020-01-04 10:04:34 +0530

    Update InterfaceNextOperation.java

--- InterfaceNextOperation.java
+++ InterfaceNextOperation.java
@@ -2,14 +2,14 @@ package interfaceNew;
 
 public class InterfaceNextOperation implements InterfaceExample1,InterfaceExp2{
 	
-public void add(int a,int b)
+public void add(int a,int b)//add 2 values
 {
 	System.out.println((a+b));
 	
 }
 public void sub(int a,int b)
 {
-	System.out.println((a-b));
+	System.out.println((a-b));//sbtract 2 values
 	
 }
 public void mult(int a,int b)
