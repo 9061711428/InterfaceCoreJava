c4f100d6fecbd23dfc261912a07e30b247790127
s@1996 <54677210+9061711428@users.noreply.github.com>
2020-01-04 10:02:51 +0530

    Interface Example2

--- InterfaceExp2.java
+++ InterfaceExp2.java
@@ -1,7 +1,7 @@
 package interfaceNew;
 
 public interface InterfaceExp2 {
-	public void mult(int a,int b);
+	public void mult(int a,int b);//declaration of method
 	public void div(int a,int b);
 
 
